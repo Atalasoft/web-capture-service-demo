3f27772355b825fbf12969166fee0e4c154d1151
guest512 <denis.legashov@gmail.com>
2018-03-21 19:28:07 +0300

    Added cache cleanup and project signing

--- Atalasoft.Demo.WebCaptureService/Handlers/ScanningHandler.ashx.cs
+++ Atalasoft.Demo.WebCaptureService/Handlers/ScanningHandler.ashx.cs
@@ -1,15 +1,59 @@
-﻿using Atalasoft.Imaging.WebControls.Capture;
+﻿using System;
+using System.Collections.Generic;
+using Atalasoft.Imaging.WebControls.Capture;
 using Atalasoft.Licensing;
 using System.Configuration;
+using System.Diagnostics;
+using System.IO;
 using System.Web;
+using System.Web.Caching;
 
 namespace Atalasoft.Demo.WebCaptureService.Handlers
 {
     public class ScanningHandler : WebCaptureRequestHandler
     {
+
         static ScanningHandler()
         {
             AtalaLicense.SetAssemblyLicense(HttpUtility.HtmlDecode(ConfigurationManager.AppSettings["AtalasoftLicenseString"]));
+            StartCacheMonitor();
         }
+
+        #region atala-capture-upload cleanup
+
+        public const string GuardFileName = "Upload Folder - Do Not Delete.txt";
+        public const string CacheFolder = "atala-capture-upload/";
+
+        private const int CacheMonitorInterval = 60;
+        private static CacheItemRemovedCallback _onCacheRemove;
+
+        public static void StartCacheMonitor()
+        {
+            _onCacheRemove = ClearExpiredFiles;
+            HttpRuntime.Cache.Insert("atala_capture_upload_cache", 0, null, DateTime.Now.AddMinutes(CacheMonitorInterval), Cache.NoSlidingExpiration, CacheItemPriority.NotRemovable, _onCacheRemove);
+        }
+
+        public static void ClearExpiredFiles(string key, object value, CacheItemRemovedReason reason)
+        {
+            foreach (var filePath in Directory.EnumerateFiles(Path.Combine(HttpRuntime.AppDomainAppPath, CacheFolder)))
+            {
+                try
+                {
+                    // Skip guard file
+                    if (Path.GetFileName(filePath).Equals(GuardFileName))
+                        continue;
+
+                    if (File.GetLastAccessTimeUtc(filePath) < DateTime.UtcNow.AddMinutes(-CacheMonitorInterval))
+                        File.Delete(filePath);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            StartCacheMonitor();
+        }
+
+        #endregion
     }
 }
